a53dcbfd65bd63fa9e5b4998ed5751acedf832eb
Leon V <leon99@outlook.com>
2020-07-27 15:10:04 +1000

    Add support for sessions

---
+++ src/Foundatio.AzureServiceBus/Queues/AzureServiceBusQueueEntryOptions.cs
@@ -0,0 +1,7 @@
+﻿using Foundatio.Queues;
+
+namespace Foundatio.AzureServiceBus.Queues {
+    public class AzureServiceBusQueueEntryOptions : QueueEntryOptions {
+        public string SessionId { get; set; }
+    }
+}
